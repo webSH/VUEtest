87782089ecbbb73b85cb3cf2bd65b2a5e85bfa0d
liliang <20386028@qq.com>
2020-10-19 16:59:15 +0800

    vue文件代码模板

---
+++ src/views/new_file.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="">
+		
+	</div>
+</template>
+<script>
+	export default {
+		name: '',
+		components: {
+			//a
+		},
+		data() {
+			return {
+				a:''
+			}
+		},
+		methods: {
+			am(){
+				
+			}
+		},
+		mounted() {
+
+		}
+	}
+</script>
+<style lang="scss" scoped>
+
+</style>
